284d8a860deea718c146e818b31783bb511b1225
John Lewis <github@jlewis.sh>
2024-05-18 17:09:07 +0200

    fix: remove unnecessary `#[allow(unused)]`

--- crates/site-app/src/helpers.rs
+++ crates/site-app/src/helpers.rs
@@ -1,6 +1,5 @@
 use leptos::*;
 
-#[allow(unused)]
 pub fn get_auth_context() -> core_types::LoggedInUser {
   // flatten Option<LoggedInUser(Option<PublicUser>)> to LoggedInUser
   core_types::LoggedInUser(
