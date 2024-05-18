3ce3834497aa1c752cae32cd9b54cbba18b92231
John Lewis <github@jlewis.sh>
2024-05-18 17:08:33 +0200

    feat: reorganized `site-app` code

---
+++ crates/site-app/src/helpers.rs
@@ -0,0 +1,9 @@
+use leptos::*;
+
+#[allow(unused)]
+pub fn get_auth_context() -> core_types::LoggedInUser {
+  // flatten Option<LoggedInUser(Option<PublicUser>)> to LoggedInUser
+  core_types::LoggedInUser(
+    use_context::<core_types::LoggedInUser>().and_then(|s| s.0),
+  )
+}
