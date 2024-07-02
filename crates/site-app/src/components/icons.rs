0ad7f6e8c476ead6fc159b792abf7ec330e6b9b8
John Lewis <github@jlewis.sh>
2024-07-02 11:02:31 -0500

    feat: added heroicons plus icon

--- crates/site-app/src/components/icons.rs
+++ crates/site-app/src/components/icons.rs
@@ -26,11 +26,10 @@ hero_icons_component_outline!(
    2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 \
    8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
 );
-
 hero_icons_component_outline!(
   HeroIconsArrowLeftStartOnRectangle,
   "M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 \
    2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15"
 );
-
 hero_icons_component_outline!(HeroIconsCheck, "m4.5 12.75 6 6 9-13.5");
+hero_icons_component_outline!(HeroIconsPlus, "M12 4.5v15m7.5-7.5h-15");
